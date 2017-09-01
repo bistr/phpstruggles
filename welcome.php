0c5e9c609a846f7dd792c793ead1d628d0fdc459
Bistra <bisi98@abv.bg>
2017-09-01 22:54:34 +0300

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,7 +1,8 @@
 <?php
+
 $servername = "sql305.byethost32.com";
 $username = "b32_20212869";
-$password = "notARealPassword";
+$password = "notreal";
 $dbname = "b32_20212869_shoutboom";
 
 // Create connection
@@ -10,13 +11,17 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
+
+
+if(isset($_POST['SubmitButton'])){
+ 
 $name=$_POST['name'];
 $email=$_POST['email'];
 $sql = "INSERT INTO shoutbox (name,post)
 VALUES ('$name', '$email')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+    echo "New record created successfully, ";
     echo $_POST["name"];
     
 
@@ -24,5 +29,43 @@ if ($conn->query($sql) === TRUE) {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-$conn->close();
+
+}
 ?>	
+
+
+<html>
+<head>
+<style>
+body{
+background-color:white;
+}
+</style>
+</head>
+<body>
+
+<form action="" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit" name="SubmitButton"/>
+</form>
+
+<?php
+$sql = "SELECT id,name,post FROM shoutbox";
+$result = $conn->query($sql);
+ 
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>Name</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["id"]."</td><td>".$row["name"]." ".$row["post"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+
+</body>
+</html>
